1bfbbf21851677d4c8ba9ddeadb5007d60ad73f1
Alessio Faieta <29785188+alessiofaieta@users.noreply.github.com>
2024-09-23 22:52:33 +0200

    Add 09-24

---
+++ 09-24/10.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+/*
+Dati in input due caratteri, entrambi maiuscoli, stampare la distanza nell'alfabeto tra i due.
+
+Esempio: la distanza tra `A` ed `F` è 5.
+*/
+int main(){
+  char x, y;
+  cin >> x >> y;
+  cout << y-x << endl;
+
+  return 0;
+}
