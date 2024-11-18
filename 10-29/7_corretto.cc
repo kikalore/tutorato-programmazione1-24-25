5e6767ea2cb882753ad3cf46c3ea0485bfd48136
kenny2817 <kenny2817.qm@gmail.com>
2024-11-18 18:37:28 +0100

    commento 7_corretto

--- 10-29/7_corretto.cc
+++ 10-29/7_corretto.cc
@@ -1,3 +1,35 @@
+/*
+Scrivere una procedura che
+
+- riempia di 1 e 0 una matrice di dimensione 10x10,
+- la stampi a video
+- dica quanti gruppi di 1 in celle vicine ci sono. Due celle sono vicine se sono sulla stessa colonna o se sono sulla stessa riga.
+
+```
+[
+  1,1,1,1,1,0,1
+  0,1,1,1,1,0,0
+  0,0,0,0,0,0,0
+  1,0,1,1,1,1,1
+  0,1,0,0,0,0,0
+  1,1,1,1,1,1,1
+  0,0,0,0,0,0,0
+]
+```
+
+In questo caso ci sono 5 gruppi di 1. In alto a sinistra c'è un gruppo di nove 1, in altro a destra c'è un gruppo di un uno, al centro a sinistra c'è un gruppo di un uno, in centro a destra c'è un
+gruppo di 5 uno, in basso c'è un gruppo di otto 1.
+
+Bonus point per dire quali sono le dimensioni dei gruppi.
+
+
+L'idea è di colorare ogni gruppo, ossia di associare ad ogni gruppo un id. Se il numero che sto controllando è zero, allora il suo id rimane lo stesso. Se invece fosse un 1, allora ci sono le seguenti
+possibilità:
+- Non ha altri vicini con un id già noto, allora è il primo 1 che incontro per quel gruppo e gli assegno un nuovo id
+- Ha vicini con un id già noto, allora quello diventa il suo id
+Per id s'intende una variabile numerica che partendo da due viene incrementata ogni volta che si incontra un nuovo gruppo
+*/
+
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
