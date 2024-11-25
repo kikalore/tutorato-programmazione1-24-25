a94070869899e874d0363dd1fbc8db8381c6b0bc
kenny2817 <kenny2817.qm@gmail.com>
2024-11-25 17:40:56 +0100

    adaptation 1.1

--- 11-12/1-1.cc
+++ 11-12/1-1.cc
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int* filter_rec(int* v, int i, int count, int& res_len){
+int* filter_rec(int* v, int i, int count, int& res_len) {
     int* res = nullptr;
-    if(i <= 0){
+    if (i <= 0) {
         res = new int[count];
     } else {
-        if(v[i - 1] % 2 == 0){
-            res = filter_rec(v, i - 1, count + 1, res_len);
+        if (v[i - 1] % 2 == 0) {
             res_len++;
-            res[res_len - 1] = v[i - 1];
+            res = filter_rec(v, i - 1, count + 1, res_len);
+            res[count] = v[i - 1];
         } else {
             res = filter_rec(v, i - 1, count, res_len);
         }
@@ -17,43 +17,43 @@ int* filter_rec(int* v, int i, int count, int& res_len){
     return res;
 }
 
-int* filter(int* v, int v_len, int& res_len){
+int* filter(int* v, int v_len, int& res_len) {
     res_len = 0;
     int* res;
-    res= filter_rec(v, v_len, 0, res_len);
+    res = filter_rec(v, v_len, 0, res_len);
     return res;
 }
 
-void print_array(int* v, int len){
+void print_array(int* v, int len) {
     cout << "Length: " << len << endl;
-    for(int i = 0; i < len; i++){
+    for (int i = 0; i < len; i++) {
         cout << v[i] << " ";
     }
-    if(len == 0){
+    if (len == 0) {
         cout << "Empty!";
     }
     cout << endl;
 }
 
-int main(){
+int main() {
     int* v;
     int v_len = 0;
-    
-    do{
+
+    do {
         cout << "Numero di elementi da inserire: ";
         cin >> v_len;
-    } while  (v_len <= 0);
+    } while (v_len <= 0);
 
     v = new int[v_len];
-    
-    for(int i = 0; i < v_len; i++){
+
+    for (int i = 0; i < v_len; i++) {
         cout << "v[" << i << "] = ";
-        cin >> v[i]; 
+        cin >> v[i];
     }
-    
+
     int res_len;
     int* res = filter(v, v_len, res_len);
-    
+
     print_array(res, res_len);
 
     delete[] res, v;
